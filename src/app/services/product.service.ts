57e1f813b39b1eb910ae298a7e82c227d1bd7cfb
Lisamontin <42090473+Lisamontin@users.noreply.github.com>
2021-01-26 11:52:01 +0100

    add get functionality

--- src/app/services/product.service.ts
+++ src/app/services/product.service.ts
@@ -1,9 +1,26 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import IProduct from 'src/app/models/IProduct'
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+  private products = new Subject<IProduct[]>();
+  $products = this.products.asObservable();
+  
+  SERVER_URL = environment.SERVER_URL;
+
+  constructor(private http: HttpClient) { }
+  
+  getAllProducts(){
+    let myObserver = {
+      next: (products) => {this.products.next(products);}
+    }
+    this.http.get(this.SERVER_URL+'/products').subscribe(myObserver);
+    console.log(this.products)
+  }
 }
