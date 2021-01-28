1d27231abba3838b2c57ac62c712759662df737d
Lisamontin <42090473+Lisamontin@users.noreply.github.com>
2021-01-28 11:36:53 +0100

    add todo comments

--- src/app/components/details/details.component.ts
+++ src/app/components/details/details.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import IProduct from 'src/app/models/IProduct';
+import { ProductService } from 'src/app/services/product.service';
+
 
 @Component({
   selector: 'app-details',
@@ -6,10 +10,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./details.component.css']
 })
 export class DetailsComponent implements OnInit {
+  productId: number;
 
-  constructor() { }
+  //product Id måste komma från ett click event från products componenten
+
+  constructor(private route: ActivatedRoute, private productService: ProductService) { }
 
   ngOnInit(): void {
-  }
+    this.productService.getProduct(this.productId) // här hämtas produkten med motsvarande url/:id 
+  } 
+
 
 }
