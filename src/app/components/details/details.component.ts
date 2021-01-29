4157a07a21dad62d4af60f5c2ec2071f47cf6f6b
Lisamontin <42090473+Lisamontin@users.noreply.github.com>
2021-01-29 19:44:08 +0100

    get and display product details

--- src/app/components/details/details.component.ts
+++ src/app/components/details/details.component.ts
@@ -11,14 +11,25 @@ import { ProductService } from 'src/app/services/product.service';
 })
 export class DetailsComponent implements OnInit {
   productId: number;
+  product: IProduct;
+  products: IProduct[];
 
-  //product Id måste komma från ett click event från products componenten
 
   constructor(private route: ActivatedRoute, private productService: ProductService) { }
 
   ngOnInit(): void {
-    this.productService.getProduct(this.productId) // här hämtas produkten med motsvarande url/:id 
-  } 
+    this.productService.$products.subscribe((products) => { this.products = products; 
+      
+      const routeParams = this.route.snapshot.paramMap;
+      const productIdFromRoute = Number(routeParams.get('id'));
+  
+      this.product = products.find(product => product.id === productIdFromRoute);
+      console.log(this.product)
+      console.log(products); });
+    this.productService.getAllProducts();
+ 
+      
+    } 
 
 
 }
