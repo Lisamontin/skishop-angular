5650f62bfc70e0ce3662e00ea58ba883650adbaa
Lisamontin <42090473+Lisamontin@users.noreply.github.com>
2021-02-03 09:25:36 +0100

    add get and post orders

--- src/app/services/order.service.ts
+++ src/app/services/order.service.ts
@@ -1,18 +1,40 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { CartService } from './cart.service';
+import { Subject } from 'rxjs';
+import { environment } from 'src/environments/environment';
+import IOrder from '../models/IOrder';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class OrderService {
-
+  SERVER_URL = environment.SERVER_URL;
   constructor(
-    private Http: HttpClient, 
-    private cartService: CartService
+    private http: HttpClient, 
   ) { }
 
-  postOrder() {
-    
+  private orders = new Subject<IOrder[]>();
+  $orders = this.orders.asObservable();
+  
+  private order = new Subject<IOrder>();
+  $order = this.order.asObservable();
+
+  
+  getOrders(){
+    let myObserver = {
+      next: (orders) => {this.orders.next(orders);}
+    }
+    this.http.get(`${this.SERVER_URL}/orders`).subscribe(myObserver);
+    console.log(this.orders, "logged from order service getorders()")
+  } 
+
+ postOrder(order: IOrder) {
+   let mySecondObserver = {
+     next: (order) => {this.order.next(order);}
+   }
+   console.log("from postOrder service")
+   return this.http.post(`${this.SERVER_URL}/Orders`, order).subscribe(mySecondObserver);
   }
+
 }
